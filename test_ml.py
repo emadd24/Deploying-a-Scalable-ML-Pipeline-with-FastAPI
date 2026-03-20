4db0a148cdb6365d74f8e3f73be84068f9e1a311
erinmaddox <emadd24@wgu.edu>
2026-03-20 15:25:25 -0600

    ADD ML unit tests

--- test_ml.py
+++ test_ml.py
@@ -1,16 +1,38 @@
 import pytest
-# TODO: add necessary import
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    assert 1 + 1 ==2
-
-
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    assert "hello".upper() == "HELLO"
+from ml.model import train_model, compute_model_metrics
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    assert len([1, 2, 3]) == 3
+# Test 1: model trains correctly
+def test_train_model():
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    y = np.array([0, 1, 0])
+
+    model = train_model(X, y)
+
+    assert isinstance(model, RandomForestClassifier)
+
+
+# Test 2: predictions match input size
+def test_inference_length():
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    y = np.array([0, 1, 0])
+
+    model = train_model(X, y)
+    preds = model.predict(X)
+
+    assert len(preds) == len(y)
+
+
+# Test 3: metrics are valid numbers
+def test_compute_model_metrics():
+    y = np.array([1, 0, 1, 1])
+    preds = np.array([1, 0, 0, 1])
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert 0 <= precision <= 1
+    assert 0 <= recall <= 1
+    assert 0 <= fbeta <= 1
